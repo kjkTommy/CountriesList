e744ee4d6f6d415723277a0190be656b9c000e23
Vladislav Kabanov <vs_kabanov@betboom.org>
2025-01-15 20:10:52 +0300

    1

--- src/app/CountryList.tsx
+++ src/app/CountryList.tsx
@@ -1,21 +1,48 @@
 'use client'
-import { CountryListProps } from '@/types'
 import React from 'react'
-import { AnimatePresence } from 'framer-motion'
+import { AnimatePresence, motion } from 'framer-motion'
+import { FixedSizeList as List } from 'react-window'
+import { CountryListProps } from '@/types'
 import CardCountry from './CardCountry'
-
 const CountryList: React.FC<CountryListProps> = ({ countries, onRemove }) => {
-    return (
-        <AnimatePresence>
-            {countries.map((country) => (
+    const Row = ({
+        index,
+        style,
+    }: {
+        index: number
+        style: React.CSSProperties
+    }) => {
+        const country = countries[index]
+        return (
+            <motion.div
+                style={style}
+                layout
+                key={country.iso_code2}
+                initial={{ opacity: 0, scale: 0.9 }}
+                animate={{ opacity: 1, scale: 1 }}
+                exit={{ opacity: 0, scale: 0.8 }}
+                transition={{ duration: 0.3 }}
+            >
                 <CardCountry
-                    key={country.iso_code2}
                     flagUrl={country.flag_url}
                     isoCode2={country.iso_code2}
                     nameRu={country.name_ru}
                     onRemove={onRemove}
                 />
-            ))}
+            </motion.div>
+        )
+    }
+
+    return (
+        <AnimatePresence>
+            <List
+                height={750}
+                itemCount={countries.length}
+                itemSize={100}
+                width="100%"
+            >
+                {Row}
+            </List>
         </AnimatePresence>
     )
 }
